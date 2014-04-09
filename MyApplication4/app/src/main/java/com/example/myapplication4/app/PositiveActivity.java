48d12179bd35541b6a9da8a590c1ed5550b3a1c1
a0r262s <a0r262s@gmail.com>
2014-04-09 16:55:08 +0200

    AlertDialog Sample App on Android Studio

---
+++ MyApplication4/app/src/main/java/com/example/myapplication4/app/PositiveActivity.java
@@ -0,0 +1,12 @@
+package com.example.myapplication4.app;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PositiveActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.positive_view);
+    }
+
+}
